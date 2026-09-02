5104b1a3d99f45cbe30c6e388256771f200140b9
Adriel Raynard <adriel.raynard@ui.ac.id>
2026-09-02 14:55:56 +0700

    test

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -24,6 +24,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# test
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
