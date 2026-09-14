6b5ad2c23bb33bcf665a95ef34fff4cbe9e25485
Adriel Raynard <adriel.raynard@ui.ac.id>
2026-09-14 20:15:33 +0700

    Fix NoReverseMatch saat membuka website

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education
+from main.views import (
+    show_main,
+    show_experience,
+    show_education,
+    download_portfolio_pdf,
+)
 
 app_name = "main"
 
@@ -8,4 +13,5 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("education/", show_education, name="show_education"),
+    path("portfolio/pdf/", download_portfolio_pdf, name="download_portfolio_pdf"),
 ]
