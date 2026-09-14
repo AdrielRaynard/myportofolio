1df0a9fef941d092916291c4a95b156c3b2f5e7c
Adriel Raynard <adriel.raynard@ui.ac.id>
2026-09-14 19:12:33 +0700

    Menambahkan unit test untuk section Education

--- main/tests.py
+++ main/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Experience, Education
 
 
 class MainTest(TestCase):
@@ -56,3 +56,63 @@ class MainTest(TestCase):
         self.assertFalse(self.experience.is_ongoing)
         self.assertContains(response, "Selesai")
         self.assertNotContains(response, "Sedang berlangsung")
+
+
+class EducationTest(TestCase):
+    def setUp(self):
+        Education.objects.all().delete()
+        self.education = Education.objects.create(
+            nama_sekolah="Universitas Contoh Testing",
+            tingkat="s2",
+            jurusan="Ilmu Komputer",
+            tahun_masuk=2023,
+            deskripsi="Menempuh studi magister sebagai bahan testing otomatis.",
+        )
+
+    def test_education_url_is_accessible_and_uses_correct_template(self):
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "education.html")
+
+    def test_education_model(self):
+        self.assertEqual(
+            str(self.education),
+            f"{self.education.get_tingkat_display()} - {self.education.nama_sekolah}",
+        )
+        self.assertTrue(self.education.is_ongoing)
+        self.assertEqual(self.education.period_display, "2023 - Sekarang")
+
+    def test_education_page_shows_data_when_available(self):
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertContains(response, self.education.nama_sekolah)
+        self.assertContains(response, self.education.jurusan)
+        self.assertContains(response, self.education.deskripsi)
+        self.assertContains(response, "Sedang berlangsung")
+        self.assertNotContains(response, "Belum ada riwayat pendidikan yang ditambahkan.")
+
+    def test_education_page_shows_empty_state_when_no_data(self):
+        Education.objects.all().delete()
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertContains(response, "Belum ada riwayat pendidikan yang ditambahkan.")
+
+    def test_completed_education_status(self):
+        self.education.tahun_lulus = 2025
+        self.education.save()
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertFalse(self.education.is_ongoing)
+        self.assertEqual(self.education.period_display, "2023 - 2025")
+        self.assertContains(response, "Selesai")
+        self.assertNotContains(response, "Sedang berlangsung")
+
+    def test_navbar_has_education_link_on_other_pages(self):
+        education_url = reverse("main:show_education")
+        main_response = self.client.get(reverse("main:show_main"))
+        experience_response = self.client.get(reverse("main:show_experience"))
+
+        self.assertContains(main_response, f'href="{education_url}"')
+        self.assertContains(experience_response, f'href="{education_url}"')
+
